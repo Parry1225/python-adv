ba25d729992393366d4da2e31a586f32fac9a8b5
Parry1225 <htst10510@htes.tp.edu.tw>
2021-08-15 11:06:12 +0800

    message

--- 20210815/神奇按鈕.py
+++ 20210815/神奇按鈕.py
@@ -4,6 +4,11 @@ from tkinter import *
 
 def hi_fun():
     print("you dide")
+    display.config(text='GGG', fg='#FFFF30', bg="#5743AB")
+
+
+def hi_fun2():
+    print("you dide")
     display.config(text="", fg='#FFFFFF', bg="#FFFFFF")
 
 
@@ -11,6 +16,8 @@ win = Tk()
 win.title('minecraft')
 btn = Button(win, text="按了會GG", command=hi_fun)
 btn.pack()
-display = Label(win, text='GGG', fg='#FFFF30', bg="#5743AB")
+btn1 = Button(win, text="按了會???", command=hi_fun2)
+btn1.pack()
+display = Label(win, text="", fg='#FFFFFF', bg="#FFFFFF")
 display.pack()
 win.mainloop()
