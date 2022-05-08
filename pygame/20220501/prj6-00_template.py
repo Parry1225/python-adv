c93f42450aa05ca89774db93d973257412392b68
Parry1225 <htst10510@htes.tp.edu.tw>
2022-05-08 08:28:49 +0800

    message

--- pygame/20220501/prj6-00_template.py
+++ pygame/20220501/prj6-00_template.py
@@ -23,12 +23,14 @@ os.chdir(sys.path[0])
 
 #===初始化設定開始===
 LIMIT_LOW = 140
+PTERA_LIMIT_LOW = 110
 LIMIT_HIGH = 140
 pygame.init()
 pygame.init()
 timer = 0
 clock = pygame.time.Clock()
 score = 0
+stuff = 0
 #***初始化設定結束***
 
 #===載入圖片開始===
@@ -37,6 +39,10 @@ img_dinosaur = [(pygame.image.load("小恐龍1.png")),
                 (pygame.image.load("小恐龍2.png"))]
 img_cacti = pygame.image.load('cacti.png')
 img_gg = pygame.image.load('gameover.png')
+img_ptera = [(pygame.image.load("翼龍飛飛1.png")),
+             (pygame.image.load("翼龍飛飛2.png"))]
+img_bend_done = [(pygame.image.load("小恐龍蹲下1.png")),
+                 (pygame.image.load("小恐龍蹲下2.png"))]
 
 #***載入圖片結束***
 bg_x = img.get_width()
@@ -64,21 +70,27 @@ def get_score(win):
 #***分數設定結束***
 
 #===恐龍設定開始===
+dino_show = img_dinosaur
+dino_limit = LIMIT_LOW
 ds_x = 50
-ds_y = LIMIT_LOW
+ds_y = dino_limit
 jumpState = False
 jumpValue = 0
 
 
+def get_dino_limit(dino_img):
+    return bg_y - 100 - dino_img[0].get_height()
+
+
 def move_dinosaur(win, timer):
-    global ds_x, ds_y, jumpValue, jumpState
+    global ds_x, ds_y, jumpValue, jumpState, dino_show, dino_limit
     if jumpState:
-        if ds_y >= LIMIT_LOW:
+        if ds_y >= dino_limit:
             jumpValue = -10
         if ds_y <= 0:
             jumpValue = 10
         ds_y += jumpValue
-        if ds_y >= LIMIT_LOW:
+        if ds_y >= dino_limit:
             jumpState = False
 
     win.blit(img_dinosaur[timer % 20 // 10], [ds_x, ds_y])
@@ -98,17 +110,43 @@ cacti_dist = int((cacti_w + cacti_h) / 2)
 def move_cacti(win):
     global cacti_x, cacti_y, cacti_shift
     global score
+    global stuff
     cacti_x -= cacti_shift
     if (cacti_x < 0):
+        stuff = random.randint(0, 1)
         score += 1
         cacti_x = bg_x - 100
-        cacti_shift = 10
+        cacti_shift = random.randint(15, 30)
     win.blit(img_cacti, [cacti_x, cacti_y])
 
     # cacti_x = (cacti_x - cacti_shift) % bg_x
 
 
 #***仙人掌設定結束***
+
+#***翼龍設定開始***
+ptera_w = img_ptera[0].get_width()
+ptera_h = img_ptera[0].get_height()
+ptera_x = bg_x - 100
+ptera_y = PTERA_LIMIT_LOW
+ptera_shift = 10
+ptera_dist = int(ptera_w + ptera_h) // 2
+
+
+def move_ptera(win, timer):
+    global ptera_x, ptera_y, ptera_shift
+    global score
+    global stuff
+    ptera_x -= ptera_shift
+    win.blit(img_ptera[timer % 20 // 10], (ptera_x, ptera_y))
+    if (ptera_x < 0):
+        stuff = random.randint(0, 1)
+        score += 1
+        ptera_x = bg_x - 100
+        ptera_shift = random.randint(10, 30)
+
+
+#***翼龍設定結束***
 
 
 #***碰撞設定結束***
@@ -149,10 +187,20 @@ while True:
             elif event.key == K_RETURN and gg == True:
                 gg = False
                 cacti_x = bg_x - 100
+                ptera_x = bg_x - 100
                 ds_x = 50
                 ds_y = LIMIT_LOW
                 score = 0
                 jumpState = False
+            elif event.key == K_DOWN and jumpState == False:
+                dino_show = img_bend_done
+                dino_limit = get_dino_limit(dino_show)
+                ds_y = dino_limit
+        elif event.type == KEYUP:
+            if event.key == K_DOWN and jumpState == False:
+                dino_show = img_dinosaur
+                dino_limit = get_dino_limit(dino_show)
+                ds_y = dino_limit
 
     #===遊戲結束===
 
@@ -167,13 +215,17 @@ while True:
         screen.blit(img, [roll_x - bg_x, 0])
         screen.blit(img, [roll_x, 0])
         move_dinosaur(screen, timer)
-        move_cacti(screen)
+        if stuff == 0:
+            move_cacti(screen)
+        else:
+
+            move_ptera(screen, timer)
         get_score(screen)
 
         if (is_hit(ds_x, ds_y, cacti_x, cacti_y, cacti_dist)):
-            print('LOL')
             gg = True
-            print('LOL')
+        if (is_hit(ds_x, ds_y, ptera_x, ptera_y, ptera_dist)):
+            gg = True
 
     pygame.display.update()
 #===主程式結束===
