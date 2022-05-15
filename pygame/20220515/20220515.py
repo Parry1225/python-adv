a951dae6ec19b9872430db0fea935c85ee19408c
Parry1225 <parry12251225@gmail.com>
2022-05-15 11:33:45 +0800

    message

--- pygame/20220515/20220515.py
+++ pygame/20220515/20220515.py
@@ -1,3 +1,4 @@
+from random import random
 import time
 from turtle import Screen
 import pygame
@@ -12,7 +13,7 @@ from pygame.locals import *
 pygame.init()
 clock = pygame.time.Clock()
 timer = 0
-
+MISSILE_MAX = 200
 #***初始化設定結束***​
 #===載入圖片開始===
 img_bg = pygame.image.load('space.png')
@@ -22,6 +23,8 @@ img_sship = [
     pygame.image.load('fighter_R.png')
 ]
 img_burn = pygame.image.load('starship_burner.png')
+img_weapon = pygame.image.load("bullet.png")
+img_enemy = pygame.image.load("enemy1.png")
 #***載入圖片結束***​
 
 #===遊戲視窗設定開始===​
@@ -85,8 +88,55 @@ def move_starship(win, key, timer):
 
 #***我機設定結束***​
 #===飛彈設定開始===
+msl_no = 0
+msl_f = [False] * MISSILE_MAX
+msl_x = [0] * MISSILE_MAX
+msl_y = [0] * MISSILE_MAX
+msl_wh = img_weapon.get_width() / 2
+msl_hh = img_weapon.get_height() / 2
+msl_shift = 30
+
+
+def move_missilc(win, key, timer):
+    global msl_f, msl_y, msl_x, msl_no
+
+    if key[K_SPACE]:
+        if timer % 10 == 0:
+            if msl_f[msl_no] == False:
+                msl_f[msl_no] = True
+                msl_x[msl_no] = ss_x - msl_wh
+                msl_y[msl_no] = ss_y - msl_hh
+                msl_no += 1
+                msl_no %= MISSILE_MAX
+    for i in range(MISSILE_MAX):
+        if msl_f[i] == True:
+            msl_y[i] -= msl_shift
+            win.blit(img_weapon, [msl_x[i], msl_y[i]])
+            if msl_y[i] < 0:
+                msl_f[i] = False
+
+
 #***飛彈設定結束***​
 #===敵機設定開始===
+emy_f = False
+emy_x = 0
+emy_y = bg_y + 10
+emy_wh = img_enemy.get_width() / 2
+emy_hh = img_enemy.get_height() / 2
+emy_shift = 5
+
+
+def move_enemy(win):
+    global emy_f, emy_x, emy_y
+    if emy_y > bg_y:
+        emy_f = True
+        emy_x = randint(emy_wh, bg_x - emy_wh)
+        emy_y = randint(emy_wh, bg_x - emy_wh)
+    if emy_f == True:
+        emy_y += emy_shift
+        win.blit(img_enemy, [emy_x - emy_wh, emy_y - emy_hh])
+
+
 #***敵機設定結束***
 #===碰撞偵測設定開始===
 #***碰撞偵測設定結束***
@@ -115,7 +165,8 @@ while True:
                 screen = pygame.display.set_mode(bg_size)
     roll_bg(screen)
     move_starship(screen, key, timer)
-
+    move_missilc(screen, key, timer)
+    move_enemy(screen)
     pygame.display.update()
 #===主程式結束===​
 
