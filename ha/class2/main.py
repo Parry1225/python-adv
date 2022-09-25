f7b4646fcd9e76baf9e6f75f680bd4451bebd907
Parry1225 <htst10510@htes.tp.edu.tw>
2022-09-25 11:27:49 +0800

    message

---
+++ ha/class2/main.py
@@ -0,0 +1,10 @@
+from machine import I2C, Pin
+from esp8266_i2c_lcd import I2cLcd
+
+f = 1000
+d = 0
+i2c = I2C(scl=Pin(5), sda=Pin(4), freq=400000)
+lcd = I2cLcd(i2c, 0x27, 2, 16)
+lcd.putstr("u suck ")
+lcd.move_to(0, 1)
+lcd.putstr("LOL")
